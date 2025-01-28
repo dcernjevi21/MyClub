5685a58e68fb51e5a713e0a52b376a80b791ea64
fkundih21 <fkundih21@student.foi.hr>
2025-01-28 16:23:58 +0100

    Initial project setup

---
+++ Software/MyClub/DataAccessLayer/Entities/Match.cs
@@ -0,0 +1,37 @@
+namespace DataAccessLayer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Match")]
+    public partial class Match
+    {
+        public int MatchID { get; set; }
+
+        public int TeamID { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime MatchDate { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string OpponentTeam { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Location { get; set; }
+
+        [StringLength(50)]
+        public string Result { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+
+        [StringLength(2000)]
+        public string Summary { get; set; }
+
+        public virtual Team Team { get; set; }
+    }
+}
