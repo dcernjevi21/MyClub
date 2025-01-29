1a9cef1562c5ffb7b70a312f4f16637bdcdeea1f
fkundih21 <fkundih21@student.foi.hr>
2025-01-29 19:59:10 +0100

    - Added Login and Registration forms

---
+++ Software/MyClub/BusinessLogicLayer/Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.EntityRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepository;
+
+        public UserService(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public int RegisterUser(User user)
+        {
+            return 1;
+        }
+    }
+}
