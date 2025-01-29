f034d2cd91806d74edd3b34a1599469a6d4aa2ef
dcernjevi21 <dcernjevi21@foi.hr>
2025-01-29 14:50:45 +0100

    Added basic structure of profile related classes

---
+++ Software/MyClub/BusinessLogicLayer/UserProfileService.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class UserProfileService
+    {
+        private bool ValidateEmail()
+        {
+            return true;
+        }
+
+        private bool ValidatePassword()
+        {
+            return true;
+        }
+
+        private void ChangeEmail()
+        {
+        }
+
+        private void ChangePassword()
+        {
+
+        }
+
+        private User GetUserByEmail(string email)
+        {
+            return new User();
+        }
+    }
+}
---
+++ Software/MyClub/DataAccessLayer/EntityRepositories/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityRepositories
+{
+    public class UserRepository
+    {
+        public int AddUser(User user)
+        {
+            return 1;
+        }
+
+        public int UpdateUser(User user)
+        {
+            return 1;
+        }
+
+        public int DeleteUser(User user)
+        {
+            return 1;
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return new List<User>();
+        }
+
+        public User GetUserByEmail(string email)
+        {
+            return new User();
+        }
+
+        public List<User> GetAllPendingUsers(User user)
+        {
+            return new List<User>();
+        }
+    }
+}
