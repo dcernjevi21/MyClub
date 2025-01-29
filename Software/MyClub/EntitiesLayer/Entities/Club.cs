cd1c9aedd6978909248453e5206ad4ae178f71f0
dcernjevi21 <dcernjevi21@foi.hr>
2025-01-29 14:50:22 +0100

    Added dependencies

--- Software/MyClub/EntitiesLayer/Entities/Club.cs
+++ Software/MyClub/EntitiesLayer/Entities/Club.cs
@@ -4,7 +4,6 @@ namespace DataAccessLayer
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     [Table("Club")]
     public partial class Club
