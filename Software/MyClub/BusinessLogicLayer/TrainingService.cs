e810c349f8ea231fa03498624a6388e4d1906238
evalec21 <evalec21@student.foi.hr>
2025-01-30 14:53:58 +0100

    Repositories and service made.

---
+++ Software/MyClub/BusinessLogicLayer/TrainingService.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.EntityRepositories;
+using EntitiesLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    class TrainingService
+    {
+        public List<Training> GetTrainings()
+        {
+            using (var repo = new TrainingRepository())
+            {
+                List<Training> trainings = repo.GetAllTrainings().ToList();
+            }
+        }
+    }
+}
