0a579d597c6b6290890d196060cd15236a358df8
dcernjevi21 <dcernjevi21@foi.hr>
2025-02-03 19:09:10 +0100

    Added tests for edit user profile case

---
+++ Software/MyClub/Tests/EditProfileTests.cs
@@ -0,0 +1,70 @@
+﻿using BusinessLogicLayer;
+using DataAccessLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PresentationLayer.UserControls;
+using System;
+using System.Reflection;
+using System.Windows.Controls;
+
+namespace Tests
+{
+    [TestClass]
+    public class EditProfileTests
+    {
+        private User _testUser;
+
+        public void Setup()
+        {
+            _testUser = new User
+            {
+                Email = "old@example.com",
+                Password = "OldPass123",
+                ProfilePicture = null
+            };
+        }
+
+        [TestMethod]
+        public void ValidateEmail_ValidEmailFormat_ReturnTrue()
+        {
+            // Arrange
+            var email = "test@gmail.com";
+            //Act
+            var userProfileService = new UserProfileServices();
+            //Assert
+            Assert.IsTrue(userProfileService.ValidateEmail(email));
+        }
+
+        [TestMethod]
+        public void ValidateEmail_InvalidEmailFormat_ReturnFalse()
+        {             
+            // Arrange
+            var email = "testgmail.com";
+            //Act
+            var userProfileService = new UserProfileServices();
+            //Assert
+            Assert.IsFalse(userProfileService.ValidateEmail(email));
+        }
+
+        [TestMethod]
+        public void ValidatePassword_ValidPasswordFormat_ReturnTrue()
+        {
+            // Arrange
+            var password = "TestPass123";
+            //Act
+            var userProfileService = new UserProfileServices();
+            //Assert
+            Assert.IsTrue(userProfileService.ValidatePassword(password));
+        }
+
+        [TestMethod]
+        public void ValidatePassword_InvalidPasswordFormat_ReturnFalse()
+        {
+            // Arrange
+            var password = "testpass";
+            //Act
+            var userProfileService = new UserProfileServices();
+            //Assert
+            Assert.IsFalse(userProfileService.ValidatePassword(password));
+        }
+    }
+}
